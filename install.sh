eef6bfe165271bcf8f303e995abdf0bf007ab555
iyaki <me@iyaki.ar>
2025-02-18 23:23:43 -0300

    Instalacion de herramientas básicas

--- install.sh
+++ install.sh
@@ -6,3 +6,27 @@ ln -s -f "$HOME/.dotfiles/.bash_functions" $HOME/.bash_functions
 ln -s -f "$HOME/.dotfiles/.bashrc" $HOME/.bashrc
 ln -s -f "$HOME/.dotfiles/.gitconfig" $HOME/.gitconfig
 ln -s -f "$HOME/.dotfiles/.vimrc" $HOME/.vimrc
+
+if [ "$(id -u)" -eq "0" ]
+then
+    if type apt-get >/dev/null 2>&1
+    then
+        apt-get update &&
+
+        apt-get install \
+            --assume-yes \
+            --no-install-recommends \
+            openssh \
+            vim
+
+        apt-get clean
+        apt-get dsitclean
+        apt-get autopurge
+    elif type brew >/dev/null 2>&1
+    then
+        brew install openssh
+        brew install vim
+
+        brew cleanup --prune --scrub
+    fi
+fi
